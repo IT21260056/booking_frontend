954070990dd223a7848cbe8cdf03a710228c11d9
viduthranaweera <it21260056@my.sliit.lk>
2023-04-18 01:08:34 +0530

    booking

--- src/user_booking/AddBooking.js
+++ src/user_booking/AddBooking.js
@@ -1,31 +1,73 @@
 import React from 'react'
 import './BookingRoom.css';
 
-export default function AddBooking() {
-    return <div className="container">
-        <div className="row">
-            <div className="body1">
 
-                <h1 className="welcome">Welcome!!</h1>
-                <div className="rectangle">
-                    <div/>
-                    <div className="text">We can assist you if you need to reserve a space for an upcoming event or
-                        arrange your stay with us. You will have a quick and simple booking experience with our system
-                        since it is user-friendly. We appreciate you giving us some thought, and we hope to have you as
-                        our guest soon!
-                    </div>
+export default function AddBooking() {
+
+
+    let selectedOption;
+
+    function handleOptionChange() {
+
+    }
+
+    return <div className="container/fluid">
+        {/*<div className="row">*/}
+        <div className="body1">
+
+            <h1 className="welcome">Welcome!!</h1>
+            <div className="rectangle">
+
+                <div className="text">We can assist you if you need to reserve a space for an upcoming event or
+                    arrange your stay with us. You will have a quick and simple booking experience with our system
+                    since it is user-friendly. We appreciate you giving us some thought, and we hope to have you as
+                    our guest soon!
                 </div>
             </div>
+        </div>
 
-            <div className="body2"></div>
+        <div className="body2">
 
-            <div className="mb-3">
-                <label htmlFor="Name" className="form-label">
-                    Name
-                </label>
-            </div>
+            <div className="container1">
+            <div className="rec2">
+                <div className="mb-3">
+                    <label htmlFor="Name" className="form-label">
+                        Name
+                    </label>
+                    <input
+                        type={"text"}
+                        className="form-control"
+                        placeholder="Your name"
+                        name="name"/>
+                </div>
+
+
+                <div className="mb-3">
+                    <label htmlFor="CheckInDate" className="form-label">
+                        Check In date
+                    </label>
+
+                    <input
+                        type={"date"}
+                        className="form-control"
+                        placeholder="Check in date"
+                        name="inDate"/>
+                </div>
+
+
+                <div className="mb-3">
+                    <select value={selectedOption} onChange={handleOptionChange}>
+                        <option value="option1">Luxury</option>
+                        <option value="option2">AC</option>
+                        <option value="option3">Non-AC</option>
+                    </select>
+                </div>
 
 
         </div>
+        </div>
+
+
+    </div>
     </div>
 }
