593d41a50b72e3a0db522b34d158e390bd1e2258
viduthranaweera <it21260056@my.sliit.lk>
2023-04-19 17:51:00 +0530

    booking

--- src/pages/Home_room.js
+++ src/pages/Home_room.js
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios';
-import {Link} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 
 export default function Home(){
 
     const [users,setUsers]=useState([])
+    const {id}=useParams()
+
     useEffect(() => {
         loadUsers();
 
@@ -12,6 +14,11 @@ export default function Home(){
     const loadUsers=async ()=>{
         const result =await axios.get("http://127.0.0.1:8080/users");
         setUsers(result.data);
+    }
+
+    const deleteUser=async(id)=>{
+        await axios.delete(`http://localhost:8080/user/${id}`)
+        loadUsers()
     }
     
 
@@ -50,13 +57,15 @@ export default function Home(){
                                 <td>{user.noOfRooms}</td>
 
                                 <td>
-                                    <button className="btn btn-primary mx-2">View</button>
-                                    <Link className="btn btn-outline-primary mx-2"
+                                    <button className="btn btn-success mx-2">View</button>
+                                    <Link className="btn btn-primary mx-2"
                                           to={`/edituser/${user.id}`}
 
-                                    >
-                                        Edit</Link>
-                                    <button className="btn btn-danger mx-2">Delete</button>
+                                    >Edit</Link>
+                                    <button className="btn btn-danger mx-2"
+
+                                    onClick={()=>deleteUser(user.id)}
+                                    >Delete</button>
                                 </td>
                             </tr>
 
