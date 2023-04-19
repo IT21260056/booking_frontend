60622c33d413134272a0ea3aef5c77209d678b16
viduthranaweera <it21260056@my.sliit.lk>
2023-04-20 01:29:49 +0530

    booking

--- src/layout/navbar.js
+++ src/layout/navbar.js
@@ -1,25 +1,39 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import './navbar.css';
 
 export default function
 
     () {
-    return (<div>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    return (
+        <div className="nav">
+            <div class="navbar navbar-expand-lg navbar-dark bg">
+
                 <div class="container-fluid">
                     <a class="navbar-brand" href="https://www.youtube.com/">
                         FRESCO
                     </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                        
-                    </button>
-                    <Link className="btn btn-outline-light" to="/user_booking">AddBooking</Link>
+                    <div className="nav-contain">
 
+                        <a className="navbar-brand" href="https://www.youtube.com/">
+                            Home
+                        </a>
+
+                        <a className="navbar-brand" href="https://www.youtube.com/">
+                            Booking
+                        </a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+
+                        </button>
+                        <Link className="btn btn-outline-light" to="/user_booking">AddBooking</Link>
+
+                    </div>
                 </div>
-            </nav>
+            </div>
 
-        </div>)
+        </div>
+    )
 }
